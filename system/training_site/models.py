f98f69c88ddd6561a9b1ead75dc89a1e7e2d5ffa
Мурад Камылов <kamylov@bk.ru>
2023-09-23 15:33:10 +0400

    add Product and Access models

--- system/training_site/models.py
+++ system/training_site/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Access(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    access = models.BooleanField(default=True)
