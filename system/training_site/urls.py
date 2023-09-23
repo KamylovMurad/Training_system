b7a943462f6a52d622267482da7f5819c4d8e19a
Мурад Камылов <kamylov@bk.ru>
2023-09-24 00:35:32 +0400

    new url product_stats

--- system/training_site/urls.py
+++ system/training_site/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
+
 from .views import (
     AllLessonView,
-    ProductLessonView
+    ProductLessonView,
+    ProductStatsView
 )
+
 
 app_name = 'training_site'
 
 urlpatterns = [
     path('all_lessons/', AllLessonView.as_view()),
-    path('product_lessons', ProductLessonView.as_view())
+    path('lessons/<str:product_name>/', ProductLessonView.as_view()),
+    path('product_stats/', ProductStatsView.as_view())
 ]
